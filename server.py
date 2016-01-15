03036d5f1ac41609a895c12040faf953d4412fef
Shun Git <kwokshungit@gmail.com>
2016-01-15 13:58:47 +0000

    fix 404

--- server.py
+++ server.py
@@ -3,6 +3,23 @@ from urllib.parse import urlsplit, parse_qs
 
 from timetable import get_timetable
 
+def get_params( qs ):
+
+    p = {}
+    q = parse_qs( qs )
+
+    if not q:
+        p['subject_codes'] = qs.split(',')
+    else:
+        if 'codes' in q:
+            codes = q['codes']
+            if len(codes) == 1: codes = codes[0].split(',')
+            p['subject_codes'] = codes
+        
+    if 'subject_codes' not in p: p['subject_codes'] = []
+
+    return p
+
 class CalHandler( http.server.SimpleHTTPRequestHandler ):
 
     def do_HEAD(s):
@@ -13,37 +30,23 @@ class CalHandler( http.server.SimpleHTTPRequestHandler ):
     def do_GET(s):
         p = urlsplit( s.path )
 
-        if p.path == '/':
-            with open( 'index.html' ) as f: page = f.read()
-
-        elif p.path == '/ics':
-            q = parse_qs( p.query )
-            if not q:
-                codes = p.query
-            else:
-                codes = q.get('codes',[''])
-                if len(codes) == 1: codes = codes[0]
-                
-            codes = codes.split(',')
-
+        if p.path == '/ics':
             s.send_response(200)
             s.send_header("Content-type", "text/calendar")
             s.end_headers()
-            cal = get_timetable( codes )
-            s.wfile.write(cal)
-        else:
-            page = "<html><head><title>error</title></head><body>%s</body></html>" % repr(p)
 
-        if page:
+            cal = get_timetable( **get_params( p.query ) )
+            s.wfile.write(cal)
+
+        else:
+            if p.path == '/':
+                with open( 'index.html' ) as f: page = f.read()
+            else:
+                s.send_error(404)
+
             s.send_response(200)
             s.send_header("Content-type", "text/html")
             s.end_headers()
-            s.wfile.write(page.encode())
-        else:
-            s.send_response(404)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            page = "<html><head><title>error</title></head><body>404</body></html>"
             s.wfile.write(page.encode())
 
 def run( host='' ):
