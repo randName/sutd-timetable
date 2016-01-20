481e535be0a3e88bac05c1d91511a217bebab5b5
Shun Git <kwokshungit@gmail.com>
2016-01-20 17:07:27 +0000

    rewrite json storage

--- timetable.py
+++ timetable.py
@@ -5,41 +5,38 @@ from collections import defaultdict
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-modulepath = "data/modules"
-locationfile = "data/SUTD_locations"
+DATA_DIR = "data"
+MOD_FILE = "modules"
+LOC_FILE = "SUTD_locations"
 
 def readjson( path ):
     """Read JSON from file."""
     try:
-        with open( path ) as f: return json.loads( f.read() )
+        with open( "%s/%s" % ( DATA_DIR, path ) ) as f: return json.loads( f.read() )
     except EnvironmentError:
         return None
 
-def writejson( js ):
+def writejson( path, js ):
     """Write to JSON file."""
-    mod = js['title'].split(' - ')[0].replace(' ','')
-    os.makedirs( "%s/%s" % ( modulepath, mod ), exist_ok=True )
-
-    with open( "%s/%s/%s" % ( modulepath, mod, js['section'] ), 'w' ) as f:
+    with open( "%s/%s" % ( DATA_DIR, path ), 'w' ) as f:
         print( json.dumps( js ), file=f )
+
+def writemod( code, section, schedule ):
+    os.makedirs( "%s/%s" % ( DATA_DIR, code ), exist_ok=True )
+    writejson( "%s/%s" % ( code, section ), schedule )
 
 def get_location_id( locations, location ):
     for k, v in locations.items():
         if v == location: return k
+    return location
 
 def parse_html( page ):
 
-    def get_times( tstring ):
-        treg = re.compile(r'([\d]+):([\d]+)(AM|PM)?')
+    def get_time( t ):
+        tm = list( map( int, t.group(1,2) ) )
+        if t.group(3) == "PM" and tm[0] < 12: tm[0] += 12
 
-        times = []
-
-        for traw in treg.finditer( tstring ):
-            tm = list(map(int,traw.group(1,2)))
-            if traw.group(3) and traw.group(3) == "PM" and tm[0] < 12: tm[0] += 12
-            times.append(tm)
-
-        return times
+        return tm
 
     def get_row( row ):
         tag = re.compile(r'MTG_([^$]+)')
@@ -54,22 +51,24 @@ def parse_html( page ):
 
         return iz
 
-    wks = ( 'description', 'start', 'end', 'location' )
-
-    locations = readjson( locationfile )
-
     soup = BeautifulSoup( page, 'html.parser' )
 
     table = soup.find('table',id='ACE_STDNT_ENRL_SSV2$0')
 
     if not table: return None
 
+    time_str = re.compile(r'([\d]+):([\d]+)(AM|PM)?')
+    locations = readjson( LOC_FILE )
+    modules = readjson( MOD_FILE )
+
     processed = []
 
     for mod in table.find_all('table',class_="PSGROUPBOXWBO"):
-        schedule = []
+        schedule = defaultdict(list)
 
-        title = mod.td.string
+        title = mod.td.string.split(' - ')
+        code = title[0].replace(' ','')
+        title = title[1]
 
         for row in mod.find_all('tr',id=re.compile(r'^trCLASS_MTG_VW')):
             iz = get_row( row )
@@ -77,23 +76,35 @@ def parse_html( page ):
             if 'SECTION' in iz: section = iz['SECTION']
             if iz['COMP'] != '\xa0': component = iz['COMP']
             dt = [ int(i) for i in reversed( iz['DATES'].split(' - ')[0].split('/') ) ]
-            tm = get_times( iz['SCHED'] )
+            tm = [ get_time( t ) for t in time_str.finditer( iz['SCHED'] ) ]
 
             info = {
-                'section': section, 'description': component,
+                'description': component,
                 'start': dt + tm[0], 'end': dt + tm[1],
                 'location': get_location_id(locations,iz['LOC']),
             }
 
-            schedule.append( info )
+            schedule[section].append( info )
 
-        dd = defaultdict(list)
+        for k, v in schedule.items(): writemod( code, k, v )
 
-        for item in schedule: dd[item['section']].append({ k: item[k] for k in wks })
+        nw = "{:%Y/%m/%d}".format( datetime.now() )
 
-        for k, v in dd.items(): writejson({ 'title': title, 'section': k, 'schedule': v })
+        if code not in modules:
+            modules[code] = { 'title': title, 'sections': [] }
+            current_sections = []
+        else:
+            current_sections = [ s[0] for s in modules[code]['sections'] ]
 
-        processed.append( ( title, ', '.join(dd.keys()) ) )
+        for section in schedule.keys():
+            if section in current_sections:
+                modules[code]['sections'][current_sections.index(section)][1] = nw
+            else:
+                modules[code]['sections'].append( ( section, nw ) )
+
+        processed.append( ( "%s - %s" % ( code, title ), ', '.join(schedule.keys()) ) )
+
+    writejson( MOD_FILE, modules )
 
     return processed
 
@@ -122,7 +133,8 @@ def get_timetable( subject_codes, name="Timetable", description="" ):
         'x-wr-caldesc': description,
     }
 
-    locations = readjson( locationfile )
+    locations = readjson( LOC_FILE )
+    modules = readjson( MOD_FILE )
 
     getloc = lambda l: "%s (%s)" % ( locations.get(l,"TBD"), l )
 
@@ -130,15 +142,16 @@ def get_timetable( subject_codes, name="Timetable", description="" ):
 
     for k, v in caldict.items(): cal.add(k,v)
 
-    for code in subject_codes:
-        s = readjson( "%s/%s" % ( modulepath, code ) )
-        if not s: continue
-        for l in s['schedule']: cal.add_component( get_event( s['title'], s['section'], l ) )
+    for code, section in subject_codes:
+        schedule = readjson( "%s/%s" % ( code, section ) )
+        if not schedule: continue
+        title = "%s - %s" % ( code, modules[code]['title'] )
+        for l in schedule: cal.add_component( get_event( title, section, l ) )
 
     return cal.to_ical()
 
 if __name__ == '__main__':
     codes = []
-    for root, subdirs, files in os.walk( modulepath ):
+    for root, subdirs, files in os.walk( DATA_DIR ):
         codes.extend([ os.path.join( os.path.basename( root ), f ) for f in files ])
     print( codes )
