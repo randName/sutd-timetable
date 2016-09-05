9799cd6bbe097e6317b9d1e414f8b8355ab68898
Shun Git <kwokshungit@gmail.com>
2016-09-05 16:29:46 +0000

    update runtime and requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.redis import FlaskRedis
-from redis import StrictRedis
-
-
-class DecodedRedis(StrictRedis):
-    @classmethod
-    def from_url(cls, url, db=None, **kwargs):
-        kwargs['decode_responses'] = True
-        return StrictRedis.from_url(url, db, **kwargs)
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_redis import FlaskRedis
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-rd = FlaskRedis.from_custom_provider(DecodedRedis, app)
+rd = FlaskRedis(app, decode_responses=True)
 
 from app import views, models
