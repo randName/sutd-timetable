c240ddac09ca407ce4f70223331d51fdfdf98b2a
Shun Git <kwokshungit@gmail.com>
2018-09-28 00:04:10 +0800

    rewrite: Firebase Cloud Functions

---
+++ README.md
@@ -0,0 +1,30 @@
+# SUTD Timetable
+A .ics generator for timetables.
+
+Fancy new serverless version.
+
+Pull requests welcome!
+
+## Todo
+- Everything
+
+## FAQ
+- [Where is my timetable?](#where-is-my-timetable)
+- [Can I request for new features?](#can-i-request-for-new-features)
+- [I found a bug, how do I tell you?](#i-found-a-bug-how-do-i-tell-you)
+- [I was late because the timetable was wrong!](#i-was-late-because-the-timetable-was-wrong)
+
+### Where is my timetable?
+Borrowing the often used analogy of functions as factories (and timetables as the products), this is not a factory or the output of the factory; this is merely the blueprint for building a factory.
+
+### That's so dumb! I just want my timetable!
+Ok.
+
+### Can I request for new features?
+Yes, please check if it's not already in my [todo](#todo), then submit an issue. That is not a guarantee that I will eventually implement it. If you do not have a GitHub account, you will have to find some other way to contact me.
+
+### I found a bug, how do I tell you?
+Submit an issue. See above note if you do not have a GitHub account.
+
+### I was late because the timetable was wrong!
+Not a question. If you suspect there was a bug that caused it, see the answer to the question above.
---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const icalendar = require('icalendar');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.database();
+const sref = db.ref('sections');
+const lref = db.ref('locations');
+
+exports.calendar = functions.https.onRequest(async (req, res) => {
+  const q = req.query;
+  const s = (q.s || '').split(',');
+  const term = (await sref.child(q.t || '-').once('value')).val() || {};
+  const sections = Object.entries(term).filter((i) => s.includes(i[0]));
+
+  const cal = new icalendar.iCalendar(true);
+  cal.addProperty('VERSION', '2.0');
+  cal.addProperty('CALSCALE', 'GREGORIAN');
+  cal.addProperty('X-WR-CALNAME', 'Timetable');
+  cal.addProperty('X-WR-CALDESC', `Schedule (${q.t})`);
+  cal.addProperty('X-WR-TIMEZONE', 'Asia/Singapore');
+  cal.addProperty('PRODID', '-//SUTD Timetable Calendar//randName//EN');
+
+  sections.map((i) => i[1]).forEach((s) => {
+    s.schedule.forEach((l) => {
+      let evt = cal.addComponent('VEVENT');
+      evt.setSummary(s.module);
+      evt.setLocation(l.location || 'TBC');
+      evt.setDate(new Date(l.start), new Date(l.end));
+      evt.setDescription(`${l.component} (${s.name})`);
+    });
+  });
+
+  res.type('text/calendar');
+  res.status(200).send(cal.toString());
+});
