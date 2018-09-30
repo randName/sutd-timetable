355591129714f0f0e8a6804bc2537947e0ac163d
Shun Git <kwokshungit@gmail.com>
2018-09-30 13:16:57 +0800

    update schema

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,13 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 const db = admin.database()
+const mref = db.ref('modules')
 const sref = db.ref('sections')
-// const lref = db.ref('locations')
 
 exports.calendar = functions.https.onRequest(async (req, res) => {
   const q = req.query
   const s = (q.s || '').split(',')
+  const mods = (await mref.once('value')).val()
   const term = (await sref.child(q.t || '-').once('value')).val() || {}
   const sections = Object.entries(term).filter((i) => s.includes(i[0]))
 
@@ -22,9 +23,11 @@ exports.calendar = functions.https.onRequest(async (req, res) => {
   cal.addProperty('PRODID', '-//SUTD Timetable Calendar//randName//EN')
 
   sections.map((i) => i[1]).forEach((s) => {
+    const m = s.module
+    const title = `${m} - ${mods[m.replace('.', '-')].title}`
     s.schedule.forEach((l) => {
       let evt = cal.addComponent('VEVENT')
-      evt.setSummary(s.module)
+      evt.setSummary(title)
       evt.setLocation(l.location || 'TBC')
       evt.setDate(new Date(l.start), new Date(l.end))
       evt.setDescription(`${l.component} (${s.name})`)
